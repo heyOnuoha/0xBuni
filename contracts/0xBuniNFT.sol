b012adfd4480b295b210a58b734380822c562350
Favour <favouronu@gmail.com>
2022-03-02 03:05:23 +0000

    new

--- contracts/0xBuniNFT.sol
+++ contracts/0xBuniNFT.sol
@@ -2,9 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import "https://github.com/heyOnuoha/ERC721A/blob/main/contracts/ERC721A.sol";
+import "https://github.com/chiru-labs/ERC721A/blob/main/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract OxBuniNFT is ERC721A, Ownable {
@@ -13,7 +12,6 @@ contract OxBuniNFT is ERC721A, Ownable {
     address public contractOwner;
 
     bool public paused = false;
-    bool public pausedFreeMint = false;
 
     uint256 public mintPrice = 0.07 ether;
     uint256 public preMintPrice = 0.05 ether;
@@ -24,23 +22,18 @@ contract OxBuniNFT is ERC721A, Ownable {
     uint256 public supply = 2000;
     uint256 public teamSupply = 70;
 
+    mapping(address => bool) public teamMinted;
+
     bool public revealed = false;
 
-    string private unRevealedURI = "";
-    string private baseURI = "";
+    string private unRevealedURI = "ipfs://QmdNszfEPEbdWWHp6uaJDhYatPsae2cWffs3Ag4vJPtu7n/1.json";
+    string private baseURI = "ipfs://QmQM46aq4sfhHoaz1Z3Wh2pZmDc7WF9TiLmrTCLvPqf41F/";
 
-    address public metaAlienContractAddress = 0xD36da67D3ABF056a32815484879d817596d37F99;
-    IERC721 public metaAlienContract;
+    address public teamWallet = 0x37673969aab5FE047843579e7592Dc295ea411ac;
 
-    mapping(address => bool) public metaAlienMinters;
-    uint256 metaAliensFreeMintCount = 0;
-    uint256 metaAliensFreeMintMax = 30;
-
-    constructor() ERC721A ("0xBuni", "0XB", supply) {
+    constructor() ERC721A ("0xBuni", "0BUN") {
 
         contractOwner = msg.sender;
-
-        metaAlienContract = IERC721(metaAlienContractAddress);
     }
 
     function reveal() public onlyOwner {
@@ -60,50 +53,36 @@ contract OxBuniNFT is ERC721A, Ownable {
         return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json")) : "";
     }
 
-    //Free Mints for holders
-    function freeMintMetaAliens(uint256 quantity) external {
-
-        require(!paused, "Contract paused");
-        require(!pausedFreeMint, "Contract paused");
-        require(metaAlienContract.balanceOf(msg.sender) > 0, "You are not eligible for this free mint");
-        require(totalSupply() + quantity <= supply, "You can't mint more then the total supply");
-
-        require(!metaAlienMinters[msg.sender], "No free mints!");
-        require(metaAliensFreeMintCount <= metaAliensFreeMintMax, "Meta Aliens Free Mint Qouta Exceeded");
-
-        metaAliensFreeMintCount = metaAliensFreeMintCount + 1;
-
-        metaAlienMinters[msg.sender] = true;
-
-        _safeMint(msg.sender, quantity);
-    }
-
     function teamMint() onlyOwner external {
 
         require(totalSupply() + teamSupply <= supply, "You can't mint more then the total supply");
+        require(!teamMinted[msg.sender], "Team has already minted");
 
-        _safeMint(msg.sender, teamSupply);
+        teamMinted[msg.sender] = true;
+
+        _safeMint(teamWallet, teamSupply);
     }
 
     function mint(uint256 quantity) external payable {
 
-        require(!paused, "Contract paused");
         require(totalSupply() + quantity <= supply, "You can't mint more then the total supply");
 
-        uint256 senderSupply = balanceOf(msg.sender);
-
         if(msg.sender != owner()) {
+
+            uint256 senderSupply = balanceOf(msg.sender);
+
+            require(!paused, "Contract paused");
 
             uint256 salePrice = mintPrice;
 
             if(!revealed) {
 
-                require(quantity + senderSupply <= presaleMaxNFTPerWallet, string(abi.encodePacked("You have only ", presaleMaxNFTPerWallet - senderSupply, " available")));
+                require(quantity + senderSupply <= presaleMaxNFTPerWallet, string(abi.encodePacked("You can only mint ", presaleMaxNFTPerWallet.toString(), " NFTs at Presale")));
 
                 salePrice = preMintPrice;
             }
 
-            require(quantity + senderSupply <= publicSaleMaxNFTPerWallet, string(abi.encodePacked("You have only ", publicSaleMaxNFTPerWallet - senderSupply, " available")));
+            require(quantity + senderSupply <= publicSaleMaxNFTPerWallet, string(abi.encodePacked("You can only mint", publicSaleMaxNFTPerWallet.toString(), " NFTs at Main Sale")));
             require(msg.value >= salePrice * quantity, "Insufficient funds");
             
         }
@@ -111,7 +90,7 @@ contract OxBuniNFT is ERC721A, Ownable {
         _safeMint(msg.sender, quantity);
     }
 
-    function mintForAddress(uint256 quantity, address recipient) external payable {
+    function mintForAddress(uint256 quantity, address recipient) external payable onlyOwner {
 
         require(!paused, "Contract paused");
         require(recipient != address(0), "Invalid reciever address");
@@ -123,12 +102,12 @@ contract OxBuniNFT is ERC721A, Ownable {
 
         if(!revealed) {
 
-            require(quantity + senderSupply <= presaleMaxNFTPerWallet, string(abi.encodePacked("You have only ", presaleMaxNFTPerWallet - senderSupply, " available")));
+            require(quantity + senderSupply <= presaleMaxNFTPerWallet, string(abi.encodePacked("You can only mint ", presaleMaxNFTPerWallet.toString(), " NFTs at Presale")));
 
             salePrice = preMintPrice;
         }
 
-        require(quantity + senderSupply <= publicSaleMaxNFTPerWallet, string(abi.encodePacked("You have only ", publicSaleMaxNFTPerWallet - senderSupply, " available")));
+        require(quantity + senderSupply <= publicSaleMaxNFTPerWallet, string(abi.encodePacked("You can only mint ", publicSaleMaxNFTPerWallet.toString(), " NFTs at Main Sale")));
         require(msg.value >= salePrice * quantity, "Insufficient funds");
         
         _safeMint(recipient, quantity);
@@ -149,27 +128,31 @@ contract OxBuniNFT is ERC721A, Ownable {
         return mintPrice;
     }
 
-    function setNotRevealedUrl(string memory unRevealedURI_) public onlyOwner { unRevealedURI = unRevealedURI_; }
+    function setNotRevealedUrl(string memory unRevealedURI_) external onlyOwner { unRevealedURI = unRevealedURI_; }
 
     function _baseURI() internal view override returns (string memory) { return baseURI; }
 
     function setBaseURI(string memory baseURI_) external onlyOwner { baseURI = baseURI_; }
 
-    function setMintPrice (uint256 _newPrice) public onlyOwner { mintPrice = _newPrice; }
+    function setMintPrice (uint256 _newPrice) external onlyOwner { mintPrice = _newPrice; }
 
-    function setPaused (bool _pausedState) public onlyOwner { paused = _pausedState; }
+    function setPaused (bool _pausedState) external onlyOwner { paused = _pausedState; }
 
-    function setPausedFree (bool _pauseState) public onlyOwner { pausedFreeMint = _pauseState; }
+    function getContractBalance () external view onlyOwner returns (uint256) { return address(this).balance; }
 
-    function getContractBalance () public view onlyOwner returns (uint256) { return address(this).balance; }
+    function setPublicSaleMaxNFTPerWallet(uint256 max_) external onlyOwner { publicSaleMaxNFTPerWallet = max_; }
 
-    function setMetaAlienContractAddress(address metaAlien_) public onlyOwner { metaAlienContract = IERC721(address(metaAlien_)); }
-
-    function setPublicSaleMaxNFTPerWallet(uint256 max_) public onlyOwner { publicSaleMaxNFTPerWallet = max_; }
-
-    function getNotRevealedURL() public view returns (string memory) { return unRevealedURI;}
+    function getNotRevealedURL() external onlyOwner view returns (string memory) { return unRevealedURI; }
 
     function changeTreasury(address payable _newWallet) external onlyOwner { contractOwner = _newWallet; }
+
+    function setTeamWallet(address teamWallet_) external onlyOwner { teamWallet = teamWallet_; }
+
+    function numberMinted(address owner) public view returns (uint256) { return _numberMinted(owner); }
+
+    function totalMinted() public view returns (uint256) { return _totalMinted(); }
+
+    function exists(uint256 tokenId) public view returns (bool) { return _exists(tokenId); }
 
     function withdraw() public payable onlyOwner {
         (bool os, ) = payable(contractOwner).call{value: address(this).balance}("");
